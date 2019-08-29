38d23ae6b46f9161fc612fce1e5f498226d3b454
Ashlee Williamson <ashleecw@gmail.com>
2019-08-29 08:01:38 +0100

    added DevService and fallback methods

--- src/main/java/com/fcgl/madrid/points/controller/UserPointsController.java
+++ src/main/java/com/fcgl/madrid/points/controller/UserPointsController.java
@@ -2,6 +2,8 @@ package com.fcgl.madrid.points.controller;
 
 import com.fcgl.madrid.points.model.UserPoint;
 import com.fcgl.madrid.points.service.UserPointsService;
+import com.fcgl.madrid.points.model.InternalStatus;
+import com.fcgl.madrid.dev.DevService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 import com.fcgl.madrid.points.model.UserPoint;
 
@@ -19,6 +22,8 @@ import java.util.List;
 public class UserPointsController {
     private UserPointsService userPointsService;
 
+    DevService devService;
+
     @Autowired
     public void setPostService(UserPointsService userPointsService) {
         this.userPointsService = userPointsService;
@@ -27,6 +32,11 @@ public class UserPointsController {
     @GetMapping("/all")
     public List<UserPoint> findAll() {
         return userPointsService.findAll();
+    }
+
+    @GetMapping(value="/fallback")
+    public ResponseEntity<InternalStatus> failureWithFallback() {
+        return devService.failureWithFallback();
     }
 
 
