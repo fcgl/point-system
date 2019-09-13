a5248d8fe857062eab2dca1436d3b5ddac2553b8
Abdullah Khalil <agalalkh@gmail.com>
2019-09-13 20:28:36 +0200

    ActionPoint: remove table

--- src/main/java/com/fcgl/madrid/points/repository/ActionPointRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fcgl.madrid.points.repository;
-
-import com.fcgl.madrid.points.model.Action;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActionPointRepository extends JpaRepository<Action, Long> {
-}
