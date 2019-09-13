0a745ca016556f8ab3da6a20cbde9c5aa74edcdf
Abdullah Khalil <agalalkh@gmail.com>
2019-09-13 20:53:00 +0200

    Repositories: add helper methods to JPA

--- src/main/java/com/fcgl/madrid/points/repository/UserPointsRepository.java
+++ src/main/java/com/fcgl/madrid/points/repository/UserPointsRepository.java
@@ -4,7 +4,9 @@ import com.fcgl.madrid.points.model.UserPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserPointsRepository extends JpaRepository<UserPoint, Long> {
-
+    Optional<UserPoint> getByUserId(Long userId);
 }
