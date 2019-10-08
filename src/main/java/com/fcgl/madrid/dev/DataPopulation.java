d0c8275aadd9ff00a8b7e05a76f16d35d5bc959b
Jean Paul Torre <jean.paul.ruben@gmail.com>
2019-10-08 12:20:58 +0800

    Add emoji name to the Trophy data model, populate data for the UserTrophy for test account

--- src/main/java/com/fcgl/madrid/dev/DataPopulation.java
+++ src/main/java/com/fcgl/madrid/dev/DataPopulation.java
@@ -1,10 +1,7 @@
 package com.fcgl.madrid.dev;
 
 import com.fcgl.madrid.points.dataModel.*;
-import com.fcgl.madrid.points.repository.ActionRepository;
-import com.fcgl.madrid.points.repository.TrophyRepository;
-import com.fcgl.madrid.points.repository.UserPointHistoryRepository;
-import com.fcgl.madrid.points.repository.UserPointsRepository;
+import com.fcgl.madrid.points.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,16 +16,19 @@ public class DataPopulation {
     private TrophyRepository trophyRepository;
     private UserPointHistoryRepository userPointHistoryRepository;
     private UserPointsRepository userPointsRepository;
+    private UserTrophyRepository userTrophyRepository;
 
     @Autowired
     public DataPopulation(ActionRepository actionRepository,
                           TrophyRepository trophyRepository,
                           UserPointHistoryRepository userPointHistoryRepository,
-                          UserPointsRepository userPointsRepository) {
+                          UserPointsRepository userPointsRepository,
+                          UserTrophyRepository userTrophyRepository) {
         this.actionRepository = actionRepository;
         this.trophyRepository = trophyRepository;
         this.userPointHistoryRepository = userPointHistoryRepository;
         this.userPointsRepository = userPointsRepository;
+        this.userTrophyRepository = userTrophyRepository;
     }
 
     @Transactional
@@ -43,8 +43,23 @@ public class DataPopulation {
         actionRepository.save(recommendingToAFriend);
 
         // Trophy #1, the user has to do action #1 x3 times to earn this trophy
-        Trophy trophy = new Trophy(100, "/no/where", "King of Receipts", addingAReceipt, 3);
+        Trophy trophy = new Trophy(100, "/no/where", "King of Receipts", addingAReceipt, 3, "baby_bottle");
+        Trophy trophy2 = new Trophy(100, "/no/where", "King of Popcorn", addingAReceipt, 2, "popcorn");
+        Trophy trophy3 = new Trophy(50, "/no/where", "King of Forum", addingAReceipt, 2, "ribbon");
+        Trophy trophy4 = new Trophy(25, "/no/where", "King of Toast", addingAReceipt, 2, "balloon");
         trophyRepository.save(trophy);
+        trophyRepository.save(trophy2);
+        trophyRepository.save(trophy3);
+        trophyRepository.save(trophy4);
+
+        UserTrophy userTrophy = new UserTrophy(1L, trophy, 1);
+        UserTrophy userTrophy2 = new UserTrophy(1L, trophy2, 1);
+        UserTrophy userTrophy3 = new UserTrophy(1L, trophy3, 1);
+        UserTrophy userTrophy4 = new UserTrophy(1L, trophy4, 1);
+        userTrophyRepository.save(userTrophy);
+        userTrophyRepository.save(userTrophy2);
+        userTrophyRepository.save(userTrophy3);
+        userTrophyRepository.save(userTrophy4);
 
 
         UserPoint userPoint = new UserPoint(1L, 100,  50);
